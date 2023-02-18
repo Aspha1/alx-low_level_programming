4ec0437c5d121ae387c050b4cbc41d87c44a2a94
hakeem <asafahakeem@gmail.com>
2023-02-18 04:46:46 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits from 0 to 9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num = 9; num++)
+
+		putchar(num);
+
+	putchar('\n');
+
+	return (0);
+
+}
