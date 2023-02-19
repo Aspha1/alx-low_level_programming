3b38dc27eafe4c6a1a130ca10333387057de9e4a
hakeem <asafahakeem@gmail.com>
2023-02-19 11:23:40 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 48; num < 58; num++)
 	{
 		putchar((num % 10) + '0');
-		if (num != 9)
+		if (num != 57)
 
 		putchar(',');
-		putchar(32);
+		putchar(' ');
 	}
 
 	putchar('\n');
