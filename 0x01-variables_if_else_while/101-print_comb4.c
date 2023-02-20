92442cc152b4e287e44a0525485e5ec82cee5323
hakeem <asafahakeem@gmail.com>
2023-02-20 13:40:49 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - prints combination of three digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+
+	for (a = 0, a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			for (c = 0; b < 10; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar((a % 10) + '0');
+					putchar((b % 10) + '0');
+					putchar((c % 10) + '0');
+
+					if (a < 7 || b < 8 || c < 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+
+}
