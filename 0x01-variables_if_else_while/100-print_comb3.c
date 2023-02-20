07a4e9d46610273d413d32204294bfcb61777ecc
hakeem <asafahakeem@gmail.com>
2023-02-19 20:59:37 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,12 +13,12 @@ int main(void)
 	{
 		for (digit2 = 0; digit2 < 10; digit2++)
 
-			if (digit1 != digit2 && digit1 < digit2)
+			if (digit1 == digit2 && digit2 <= 9)
 			{
 				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
 			}
-		if (digit1 + digit2 != 17)
+		if (digit1 != 9 && digit2 != 9)
 		{
 			putchar(',');
 			putchar(' ');
