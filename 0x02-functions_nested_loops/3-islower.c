6b4f788cee2a88ce9e3e613bee469c868c27931a
hakeem <asafahakeem@gmail.com>
2023-02-22 09:28:08 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#iclude "main.h"
+
+/**
+ * _islower - print lowercase character
+ * @c: the character
+ * Return: 1 if letter is lowercase,0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 'a'; &&c <= 'z')
+		return (1);
+	else
+		return (0);
+}
