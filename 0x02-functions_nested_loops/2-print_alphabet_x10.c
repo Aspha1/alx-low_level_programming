0096c5d5591d6822cce7f8d945b70f95d041c3da
hakeem <asafahakeem@gmail.com>
2023-02-22 09:02:07 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char lc;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (lc = 'a'; lc <= 'z'; lc++)
+			_putchar(la);
+	}
+	_putchar('\n');
+}
