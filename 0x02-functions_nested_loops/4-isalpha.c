d86d2400abc4733f589343a0075d881f11e024ef
hakeem <asafahakeem@gmail.com>
2023-02-22 10:05:09 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for uppercase or lowercase
+ * @c: character to be checked
+ * Return: 1 if character is a letter, o if otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' c <= 'Z'))
+		return (i);
+	else
+		return (0);
+}
