a00094255abb35eda81a2204da0ef74f9f050cb5
hakeem <asafahakeem@gmail.com>
2023-02-23 17:18:56 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,6 +2,7 @@
 
 /**
  * print_numbers - print 0 to 9.
+ * Return: Always 0
  */
 void print_numbers(void)
 {
@@ -12,4 +13,5 @@ void print_numbers(void)
 		_putchar(x + '0');
 	}
 	_putchar('\n');
+	return (0);
 }
