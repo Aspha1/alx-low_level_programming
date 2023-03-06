3ae2c032c4cd40947998e625dff266a959eb8c1d
hakeem <asafahakeem@gmail.com>
2023-03-06 05:36:51 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,9 +9,9 @@ void print_chessboard(char (*a)[8])
 {
 	int e, f;
 
-	for (e = 0; e <= 8; e++)
+	for (e = 0; e < 8; e++)
 	{
-		for (f = 0; f <= 8; f++)
+		for (f = 0; f < 8; f++)
 			_putchar(a[e][f]);
 		_putchar('\n');
 	}
