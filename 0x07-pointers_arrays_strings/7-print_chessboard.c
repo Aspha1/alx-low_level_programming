04d0f232081203e7152781031671375f53d044bc
hakeem <asafahakeem@gmail.com>
2023-03-06 05:33:40 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints chessboard.
+ * @a: array.
+ * Return: 0 if success.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int e, f;
+
+	for (e = 0; e <= 8; e++)
+	{
+		for (f = 0; f <= 8; f++)
+			_putchar(a[e][f]);
+		_putchar('\n');
+	}
+}
